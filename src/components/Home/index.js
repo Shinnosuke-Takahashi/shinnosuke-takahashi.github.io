7c011d550824eda1740a958c48abb4bc583aa8ae
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2022-09-27 19:32:07 -0400

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/NYC_S.png';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import Loader from 'react-loaders'
+import './index.scss';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = "hinnosuke:".split("")
+    const jobArray = "Software Engineer".split("")
+    useEffect(() => {
+        setTimeout(() => {
+          return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt = "engineer"/>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx = {15}/>
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx = {22}/>
+                    </h1>
+                    <h2>Software Engineer / Web Developer</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
+            </div>
+            <Loader type="ball-scale-ripple-multiple" />
+        </>
+    );
+}
+
+export default Home
